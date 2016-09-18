afebaa2dd4b9e92f6bab6678b26c729ca02d69d5
Adam Deeley <ad.deeley@gmail.com>
2016-09-18 21:15:56 +0100

    Signed-off-by: Adam Deeley <ad.deeley@gmail.com>

--- populate_rango.py
+++ populate_rango.py
@@ -28,14 +28,16 @@ def populate():
         {"title": "Bottle",
          "url": "http://bottle.org/docs/dev/"},
         {"title": "Flask",
-         "url": "http//flask.pocoo.org"}]
+         "url": "http//flask.pocoo.org"},
+        {"title": "Learn Python the Hard Way",
+         "url": "http://learnpythonthehardway.org/"}]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}}
+    cats = {"Python": {"pages": python_pages, "views":128, "likes":64},
+            "Django": {"pages": django_pages, "views":64, "likes":32},
+            "Other Frameworks": {"pages": other_pages, "views":32, "likes":16}}
             
-    for cat, cat_data in cats.items():
-        c = add_cat(cat)
+    for cat, cat_data in cats.items(): 
+        c = add_cat(cat) # update to provide views and likes arguments?
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
             
@@ -45,13 +47,15 @@ def populate():
             
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
+    p.url=url 
     p.views=views
     p.save()
     return p
     
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views # is this correct ?
+    c.likes=likes
     c.save()
     return c
     
