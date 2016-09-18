2e0dc8c74ce4d620c80fe511eb5fe2a56ae3ecd2
adam deeley <ad.deeley@gmail.com>
2016-09-18 13:17:06 +0100

    Signed-off-by: adam deeley <ad.deeley@gmail.com>

--- rango/models.py
+++ rango/models.py
@@ -5,9 +5,12 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+	
+    class Meta:
+        verbose_name_plural = 'categories'
     
-    def __str__(self):
-        return self.name
+        def __str__(self):
+            return self.name
         
 class Page(models.Model):
     category = models.ForeignKey(Category)
