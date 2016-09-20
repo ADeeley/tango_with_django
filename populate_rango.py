1b876dc268e614eba919568d2f25239f2fedc26e
adam deeley <ad.deeley@gmail.com>
2016-09-20 22:07:33 +0100

    Updated the population script to include likes and views for each category. Signed-off-by: adam deeley <ad.deeley@gmail.com>

--- populate_rango.py
+++ populate_rango.py
@@ -32,30 +32,30 @@ def populate():
         {"title": "Learn Python the Hard Way",
          "url": "http://learnpythonthehardway.org/"}]
 
-    cats = {"Python": {"pages": python_pages, "views":128, "likes":64},
-            "Django": {"pages": django_pages, "views":64, "likes":32},
-            "Other Frameworks": {"pages": other_pages, "views":32, "likes":16}}
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages,  "views": 32, "likes": 16}}
+            
             
     for cat, cat_data in cats.items(): 
-        c = add_cat(cat) # update to provide views and likes arguments?
+        c = add_cat(cat, cat_data["views"], cat_data["likes"]) # update to provide views and likes arguments?
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
             
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
-            
-def add_page(cat, title, url, views=0):
+           
+def add_page(cat, title, url):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url 
-    p.views=views
     p.save()
     return p
     
-def add_cat(name, views, likes):
-    c = Category.objects.get_or_create(name=name)[0]
-    c.views=views # is this correct ?
-    c.likes=likes
+def add_cat(name, views, likes): # added views and likes here
+    c = Category.objects.get_or_create(name=name)[0] # something probably goes here
+    c.views = views
+    c.likes = likes
     c.save()
     return c
     
