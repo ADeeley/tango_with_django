fd3a12fd9f7a72919b625e52a5ee28575d4b3ff9
adam deeley <ad.deeley@gmail.com>
2016-09-22 19:23:03 +0100

    Signed-off-by: adam deeley <ad.deeley@gmail.com>

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+# to update the categories on the admin page to show the category names, not
+# "category Object"
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
